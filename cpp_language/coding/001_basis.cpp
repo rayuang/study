248c15784c2f761c83f995f00269c4462a5fa65f
rayuang <rayuang@163.com>
2023-01-10 00:16:53 +0800

    cpp_notes_and_coding

---
+++ cpp_language/coding/001_basis.cpp
@@ -0,0 +1,20 @@
+// 导入iostream头文件
+#include <iostream>
+// 使用命名空间std
+using namespace std;
+// #define定义宏常量
+#define MAX 10
+// 主函数-> 有且只有一个
+int main()
+{
+    cout << "HelloWord" << endl;
+
+    // 数据类型,同c
+    int num = 10;
+    cout << "num=" << num << endl;
+    // const修饰的变量-> 常量
+    const int number = 20;
+    cout << "MAX=" << MAX << "\t"
+         << "number=" << number << endl;
+    return 0;
+}
