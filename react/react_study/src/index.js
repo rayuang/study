61f2899c60a992a16b219ef71fb30e1f95a255ea
rayuang <rayuang@163.com>
2023-01-17 01:36:25 +0800

    react_coding

--- react/react_study/src/index.js
+++ react/react_study/src/index.js
@@ -3,9 +3,7 @@
  */
 import React from "react" //react核心包
 import ReactDom from "react-dom" //将react组件渲染到页面
-import ClassComponent from "./0001base/0001class_component"
-import FunctionComponent from "./0001base/0002func_component"
-import ComponentNested from "./0001base/0003component_nested"
+import App from "./0001base/0005binding_event"
 // ReactDom.render("<h1>HelloWorld</h1>",document.getElementById("root"))//将文字渲染到id为root的标签中
 // ReactDom.render(<h1>HelloWorld</h1>,document.getElementById("root"))//将文字渲染到id为root的标签中
 /*
@@ -19,14 +17,5 @@ ReactDom.render(React.createElement(
 console.log("helloworld")
 
 ReactDom.render(
-    <ClassComponent></ClassComponent>, document.getElementById("class_component")
-)
-
-
-ReactDom.render(
-    <FunctionComponent></FunctionComponent>, document.getElementById("func_component")
-)
-
-ReactDom.render(
-    <ComponentNested></ComponentNested>, document.getElementById("component_nested")
+    <App></App>, document.getElementById("root")
 )
